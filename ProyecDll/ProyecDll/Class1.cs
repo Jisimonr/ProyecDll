0d0c4847de070a26c87726c8e927e849a78b1d02
NachoSimon <jisimonr@outlook.es>
2019-11-26 09:21:03 +0100

    first commit

---
+++ ProyecDll/ProyecDll/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ProyecDll
+{
+    public class Class1
+    {
+        public int ContarPalabras(string texto)
+        {
+            return texto.Split(' ').Length;
+        }
+
+        public string Dametexto(int posicion, string texto)
+        {
+            string tomatexto = texto.Substring(posicion);
+            return "";
+        }
+    }
+}
